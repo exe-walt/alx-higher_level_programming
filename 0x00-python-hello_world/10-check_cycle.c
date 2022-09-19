3e45932945fe5e6802c7cc5e7418145012b04dfb
Paul_Adansi <paul.adansi25@gmail.com>
2022-09-20 00:03:39 +0100

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,19 +6,20 @@
  * Return: 1 if the list has a cycle 
  * Return: 0 if it doesn't
  */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *duck = list;
-	listint_t *goose = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (!list)
 		return (0);
 
-	while (duck && goose && goose->next)
+	while (slow && fast && fast->next)
 	{
-		duck = duck->next;
-		goose = fast->next->next;
-		if (duck == fast)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
 	}
 
