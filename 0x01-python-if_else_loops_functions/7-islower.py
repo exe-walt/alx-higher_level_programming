8006284c697fdf554223e6182309a217a71910a8
Paul_Adansi <paul.adansi25@gmail.com>
2022-09-20 17:47:52 +0100

    Update Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,10 +2,9 @@
 
 """Task 7 - A function that checks for lowercase characters."""
 
+
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
-        return True
-
-    
+        return True   
     else:
         return False
