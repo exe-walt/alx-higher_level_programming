77b0d96d99664720e21c93b81470048e44f7a974
Paul_Adansi <paul.adansi25@gmail.com>
2022-09-20 17:25:18 +0100

    Update Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Task 6 - A program that prints all possible different combinations of two digits."""
+"""Task 6 - A program that prints all possible 
+          different combinations of two digits."""
 
 for num1 in range(0, 10):
     for num2 in range(num1 + 1, 10):
