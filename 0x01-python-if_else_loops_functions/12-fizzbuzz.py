8ae57c601e471a4f64f8705d4717ee2da390dbe4
Paul_Adansi <paul.adansi25@gmail.com>
2022-09-20 18:33:12 +0100

    Task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Task 12 - a function that prints the numbers from 
+1 to 100 separated by a space.
+"""
+
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end='')
+        if i % 5 == 0:
+            print("Buzz", end='')
+        if i % 3 and i % 5:
+            print("{:d}".format(i), end='')
+        print(end=' ')
