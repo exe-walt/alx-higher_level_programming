6a2a72d4a6b2640fb6eb34da04473f1dd5f34c78
Paul_Adansi <paul.adansi25@gmail.com>
2022-09-20 17:33:14 +0100

    Update Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-"""Task 5 - A program that prints numbers from 0 to 99."""
+"""A program that prints numbers from 0 to 99."""
 
 for number in range(0, 100):
     if number == 99:
         print("{}".format(number))
     else:
         print("{:02}".format(number), end=", ")
-        
