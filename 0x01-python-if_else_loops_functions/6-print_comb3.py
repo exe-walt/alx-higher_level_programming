c896983500fe35b589698275895ab16a5ea1eaf1
Paul_Adansi <paul.adansi25@gmail.com>
2022-09-20 16:55:53 +0100

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Task 6 - A program that prints all possible different combinations of two digits."""
+
+for num1 in range(0, 10):
+    for num2 in range(num1 + 1, 10):
+        if num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
+        else:
+            print("{}{}".format(num1, num2), end=", ")
