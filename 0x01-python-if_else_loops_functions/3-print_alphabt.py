2fc62e899cb879f5044222a1696840f0edac3637
exe-walt <106749659+exe-walt@users.noreply.github.com>
2022-09-21 00:24:11 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+"""Task 3 - a program that prints the ASCII alphabet, in lowercase, 
+    not followed by a new line."""
+
 for a in range(ord('a'), ord('z') + 1):
     if chr(a) != 'e' and chr(a) != 'q':
         print("{:c}".format(a), end='')
