f46d5e81c6e4d12dcdade1911d1b10ada172ae47
Paul_Adansi <paul.adansi25@gmail.com>
2022-09-24 02:00:48 +0100

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    inf  = 0
+    total  = 0
     for i in sys.argv[1:]:
-        inf += int(i)
-        print("{:d}".format(inf))
+        total += int(i)
+        print("{:d}".format(total))
         
