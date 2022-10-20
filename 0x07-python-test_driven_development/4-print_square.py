7d423286593f92f8dbcfd0bda82fe69185268042
exe-walt <106749659+exe-walt@users.noreply.github.com>
2022-10-20 08:41:57 +0100

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,7 +3,6 @@
 
 
 def print_square(size):
-    
     """Print a square with the # character.
     Args:
         size (int): The height/width of the square.
@@ -11,7 +10,6 @@ def print_square(size):
         TypeError: If size is not an integer.
         ValueError: If size is < 0
     """
-    
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
